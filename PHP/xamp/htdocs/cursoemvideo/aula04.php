7def68e1ceeee9d6c3778f6ec69ca8fee1a99815
Ivan Viana de Araujo <ivanviana@hotmail.com>
2022-05-09 13:16:22 -0300

    Update aula04.php

--- PHP/xamp/htdocs/cursoemvideo/aula04.php
+++ PHP/xamp/htdocs/cursoemvideo/aula04.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estiloaulas.css">
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        $n = 4;
-        $no = "Ivan";
-        echo $no;
-    ?>
+    <header><br></header>
+    <section>
+        <?php 
+            $n = 4;
+            $no = "Ivan";
+            echo $no;
+        ?>
+    </section>
+    
 </body>
 </html>
